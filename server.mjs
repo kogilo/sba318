5c5d5848584d58f749aeafee4ff4d25279ad6fba
kogilo <getabullaothow@gmail.com>
2024-10-11 11:02:24 -0500

    First test-server Running on PORT 3000

--- server.mjs
+++ server.mjs
@@ -1,4 +1,28 @@
 // Initialize the Project
-// npm init -y 
-// npm install express
+    // npm init -y 
+    // npm install express
+// Setup Git
 
+// Imports
+import express from 'express';
+
+// Create the Express Server /  an instance of express
+
+const app = express();
+let PORT = 3000;
+
+
+
+// const express = require('express');
+// const app = express();
+// const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send(`Server is running on port ${PORT}`);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
