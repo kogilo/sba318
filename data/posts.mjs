b2bd14822884da05ea97c5b27597db2877644c89
kogilo <getabullaothow@gmail.com>
2024-10-13 02:25:27 -0500

    All routes included in server.mjs

--- data/posts.mjs
+++ data/posts.mjs
@@ -1,24 +1,38 @@
- [
-{
-      "id": 1,
-      "title": "Introduction to Node.js",
-      "content": "Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine...",
-      "authorId": 1,
-      "comments": [1, 2]
+[
+    {
+        "id": 1,
+        "title": "Introduction to Node.js",
+        "content": "Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine...",
+        "authorId": 1,
+        "comments": [
+            1,
+            2
+        ]
     },
     {
-      "id": 2,
-      "title": "Understanding Express.js",
-      "content": "Express.js is a web framework for Node.js...",
-      "authorId": 2,
-      "comments": [3]
+        "id": 2,
+        "title": "Understanding Express.js",
+        "content": "Express.js is a web framework for Node.js...",
+        "authorId": 2,
+        "comments": [
+            3
+        ]
     },
     {
-      "id": 3,
-      "title": "RESTful API Design",
-      "content": "RESTful APIs follow a set of principles that make web services more usable...",
-      "authorId": 1,
-      "comments": []
+        "id": 3,
+        "title": "RESTful API Design",
+        "content": "RESTful APIs follow a set of principles that make web services more usable...",
+        "authorId": 1,
+        "comments": []
+    },
+    {
+        "id": 4,
+        "title": "Node.js",
+        "content": "Node awsome...",
+        "authorId": 2,
+        "comments": [
+            2,
+            3
+        ]
     }
-  ]
-  
+]
--- server.mjs
+++ server.mjs
@@ -1,16 +1,24 @@
+
 // import { readFileSync } from 'fs'; // for data reading
 // import express from 'express';
+
+// import { readFileSync } from 'fs';
+import fs, {readFileSync, readFile, writeFile } from 'fs';
+
 import path from 'path';
 import url from 'url'
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
  
  
-import { readFileSync } from 'fs';
+
+
 import express from 'express';
 
 const app = express();
 const port = 3000;
+
+app.use(express.json()); // middleware
 
 // app.get('/', (req, res) => {
 //     res.status(200).json({message: 'Hello from server side...', app: 'natour',node: 'JS env'}
@@ -19,7 +27,7 @@ const port = 3000;
 //     res.send(`You can post to this endpoint...`);
 // })
 const posts = JSON.parse(readFileSync(`${__dirname}/data/posts.mjs`));
-app.get('/api/v1/comments', (req, res) =>{
+app.get('/api/v1/posts', (req, res) =>{
     res.status(200).json({
         status: 'success',
         result: posts.length,
@@ -28,6 +36,85 @@ app.get('/api/v1/comments', (req, res) =>{
         }
     })
 })
+
+
+app.get('/api/v1/posts/:id', (req, res) =>{
+  console.log(req.params)
+
+  const id = req.params.id * 1; //convert id to number.
+  const post = posts.find(el => el.id === id);
+
+  // if(id > tours.length) {
+   if(!posts) {
+      return res.status(404),express.json({
+          status: 'fail',
+          message: 'Invalid ID'
+      });
+  }
+  
+  res.status(200).json({
+      status: 'success',
+      data: {
+          posts
+      }
+  })
+})
+
+
+app.patch('/api/v1/posts/:id', (req, res) => {
+    if(req.params.id * 1 > posts.length) {
+        return res.status(404),express.json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+    
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated post here...'
+    }
+  })
+})
+
+
+
+app.delete('/api/v1/posts/:id', (req, res) => {
+  if(req.params.id * 1 > posts.length) {
+      return res.status(404),express.json({
+          status: 'fail',
+          message: 'Invalid ID'
+      });
+  }
+  
+res.status(204).json({
+  status: 'success',
+  data: null
+})
+})
+
+
+
+
+app.post('/api/v1/posts', (req, res) =>{
+  // console.log(req.body);
+  const newId = posts[posts.length -1].id +1;
+  const newPost = Object.assign({id: newId
+
+  }, req.body);
+
+  posts.push(newPost);
+  fs.writeFile(`${__dirname}/data/posts.mjs`, JSON.stringify(posts), err => {
+      res.status(201).json({
+          status: "success",
+          data: {
+              post: newPost
+          }
+      })
+
+  });
+});
+
 
 
 
